0103b9bff8bdb60afd21c0f9774d8e2204233492
JulianChu <yulang.chu@gmail.com>
2017-06-19 13:18:01 +0200

    add passed test case: give {1,space,4} should return {1,4}

--- BLLTests/ParseControllerTests.cs
+++ BLLTests/ParseControllerTests.cs
@@ -146,5 +146,25 @@ namespace BLL.Tests
             var expected = new List<char>() { '4', '1' };
             Assert.IsTrue(actual.SequenceEqual(expected));
         }
+
+        [TestMethod()]
+        public void ParseNumberFrom2DCharList_Give_1s4_Return_1_4()
+        {
+            //Assign 
+            var data = new List<List<char>>()
+            {
+                new List<char>() { '|', ' ', '|', ' ', ' ', ' ', '|', '\r', '\n' },
+                new List<char>() { '|', ' ', '|', '_', '_', '_', '|', '\r', '\n' },
+                new List<char>() { '|', ' ', ' ', ' ', ' ', ' ', '|', '\r', '\n' },
+                new List<char>() { '|', ' ', ' ', ' ', ' ', ' ', '|' }
+            };
+            var controller = new ParseController();
+
+            //Act
+            var actual = controller.ParseNumberFrom2DCharList(data);
+            //Assert
+            var expected = new List<char>() { '1', '4' };
+            Assert.IsTrue(actual.SequenceEqual(expected));
+        }
     }
 }
