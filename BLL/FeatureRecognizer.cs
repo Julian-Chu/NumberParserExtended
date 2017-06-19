89283bfd03cc4280dd2a5b23e111bbf1c804e39a
JulianChu <yulang.chu@gmail.com>
2017-06-19 15:34:14 +0200

    add FeatureRecognizer, and move some methods from to it

---
+++ BLL/FeatureRecognizer.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BLL
+{
+    internal class FeatureRecognizer
+    {
+        public int GetWidth(char number)
+        {
+            ///Width is how many chars in 1st row of a Number 2D char array
+            ///              v---v                       v-v
+            ///Ex: Width of  |   |  is 5 ,     Width of  --- is 3
+            ///              |___|                        /         
+            ///                  |                        \        
+            ///                  |                       --
+            switch (number)
+            {
+                case '3':
+                case '2':
+                    return 3; //width of number : 3 chars
+                case '1':
+                    return 1;
+                case '4':
+                case '5':
+                    return 5;
+                default:
+                    return 1000; // warning for unknown number
+            }
+        }
+
+        public List<char> GetFeature(List<List<char>> chars, int row, int col)
+        {
+            ///feature is chars in 1st column of a Number 2D char array
+            ///Ex: features of  |   |  are  '|' ,  features of  ---   are '-'
+            ///                 |___|       '|'                  /        ' '
+            ///                     |       ' '                  \        ' '
+            ///                     |       ' '                 --        '-'
+            var feature = new List<char>();
+            feature.Add(chars[row][col]);
+            feature.Add(chars[row + 1][col]);
+            feature.Add(chars[row + 2][col]);
+            feature.Add(chars[row + 3][col]);
+            return feature;
+        }
+
+        public char GetNumberByFeatures(List<char> features)
+        {
+            if (features.SequenceEqual(new List<char>() { '-',
+                                                          ' ',
+                                                          ' ',
+                                                          '-' }))
+            {
+                return '3';
+            }
+            else if (features.SequenceEqual(new List<char> { '-',
+                                                             ' ',
+                                                             '|',
+                                                             '-' }))
+            {
+                return '2';
+            }
+            else if (features.SequenceEqual(new List<char> { '|',
+                                                             '|',
+                                                             '|',
+                                                             '|' }))
+            {
+                return '1';
+            }
+            else if (features.SequenceEqual(new List<char> { '|',
+                                                             '|',
+                                                             ' ',
+                                                             ' ' }))
+            {
+                return '4';
+            }
+            else if (features.SequenceEqual(new List<char> { '-',
+                                                             '|',
+                                                             ' ',
+                                                             '_' }))
+            {
+                return '5';
+            }
+            else
+            {
+                return '*';
+            }
+        }
+    }
+}
--- BLL/ParseController.cs
+++ BLL/ParseController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 
 namespace BLL
 {
@@ -7,6 +6,8 @@ namespace BLL
     {
         public List<char> ParseNumbers(List<List<char>> chars)
         {
+            FeatureRecognizer recognizer = new FeatureRecognizer();
+
             if (chars.Count == 1)
             {
                 return new List<char>() { 'E', 'r', 'r', 'o', 'r' };
@@ -22,10 +23,9 @@ namespace BLL
             {
                 if (targetChars.Contains(chars[row][col]))
                 {
-                    List<char> feature = GetFeature(chars, row, col);
-
-                    char number = GetNumberByFeatures(feature);
-                    col += GetWidth(number);
+                    List<char> feature = recognizer.GetFeature(chars, row, col);
+                    char number = recognizer.GetNumberByFeatures(feature);
+                    col += recognizer.GetWidth(number);
 
                     result.Add(number);
                 }
@@ -41,91 +41,10 @@ namespace BLL
                     col++;
                 }
             }
-
             result.RemoveRange(result.Count - 2, 2);
 
             return result;
         }
 
-        private int GetWidth(char number)
-        {
-            ///Width is how many chars in 1st row of a Number 2D char array
-            ///              v---v                       v-v
-            ///Ex: Width of  |   |  is 5 ,     Width of  --- is 3
-            ///              |___|                        /         
-            ///                  |                        \        
-            ///                  |                       --
-            switch (number)
-            {
-                case '3':
-                case '2':
-                    return 3; //width of number : 3 chars
-                case '1':
-                    return 1;
-                case '4':
-                case '5':
-                    return 5;
-                default:
-                    return 1000; // warning for unknown number
-            }
-        }
-
-        private List<char> GetFeature(List<List<char>> chars, int row, int col)
-        {
-            ///feature is chars in 1st column of a Number 2D char array
-            ///Ex: features of  |   |  are  '|' ,  features of  ---   are '-'
-            ///                 |___|       '|'                  /        ' '
-            ///                     |       ' '                  \        ' '
-            ///                     |       ' '                 --        '-'
-            var feature = new List<char>();
-            feature.Add(chars[row][col]);
-            feature.Add(chars[row + 1][col]);
-            feature.Add(chars[row + 2][col]);
-            feature.Add(chars[row + 3][col]);
-            return feature;
-        }
-
-        private char GetNumberByFeatures(List<char> features)
-        {
-            if (features.SequenceEqual(new List<char>() { '-',
-                                                          ' ',
-                                                          ' ',
-                                                          '-' }))
-            {
-                return '3';
-            }
-            else if (features.SequenceEqual(new List<char> { '-',
-                                                             ' ',
-                                                             '|',
-                                                             '-' }))
-            {
-                return '2';
-            }
-            else if (features.SequenceEqual(new List<char> { '|',
-                                                             '|',
-                                                             '|',
-                                                             '|' }))
-            {
-                return '1';
-            }
-            else if (features.SequenceEqual(new List<char> { '|',
-                                                             '|',
-                                                             ' ',
-                                                             ' ' }))
-            {
-                return '4';
-            }
-            else if (features.SequenceEqual(new List<char> { '-',
-                                                             '|',
-                                                             ' ',
-                                                             '_' }))
-            {
-                return '5';
-            }
-            else
-            {
-                return '*';
-            }
-        }
     }
 }
