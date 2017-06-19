826e6179a3555806750c5744be2dfb8ce157f9e9
JulianChu <yulang.chu@gmail.com>
2017-06-19 14:18:31 +0200

    add ShowParsedResult() in Program.cs

--- NumberParserExtended/Program.cs
+++ NumberParserExtended/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using BLL;
 using DAL;
 
 namespace NumberParserExtended
@@ -7,18 +9,44 @@ namespace NumberParserExtended
     {
         static void Main(string[] args)
         {
-            DataReader reader = new DataReader(@"c:\users\yulan\documents\visual studio 2015\Projects\NumberParserExtended\DAL\NumberParserExtended_Simplified.txt");
-            var result = reader.GetData();
+            string filePath = @"c:\users\yulan\documents\visual studio 2015\Projects\NumberParserExtended\DAL\NumberParserExtended_Simplified.txt";
+            filePath = null;
+            ShowDataFromTxt(filePath);
+            Console.WriteLine();
 
-            foreach (var row in result)
+            var result = GetParsedResult(filePath);
+            foreach (var ch in result)
+            {
+                Console.Write(ch);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static List<char> GetParsedResult(string filePath)
+        {
+            DataReader reader = new DataReader(filePath);
+            var data = reader.GetData();
+
+
+            ParseController controller = new ParseController();
+            var result = controller.ParseNumberFrom2DCharList(data);
+
+            return result;
+        }
+
+        private static void ShowDataFromTxt(string filePath)
+        {
+            DataReader reader = new DataReader(filePath);
+            var data = reader.GetData();
+
+            foreach (var row in data)
             {
                 foreach (var ch in row)
                 {
                     Console.Write(ch);
                 }
             }
-
-            Console.ReadKey();
 
         }
     }
