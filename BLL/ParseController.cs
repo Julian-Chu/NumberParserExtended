12df6ba5d7d6665098bf3bd327107b1d1ea6e66d
JulianChu <yulang.chu@gmail.com>
2017-06-19 12:06:16 +0200

    passed test case: give 3 return 3

--- BLL/ParseController.cs
+++ BLL/ParseController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace BLL
 {
@@ -9,9 +10,37 @@ namespace BLL
             var result = new List<char>();
             int row = 0;
             int col = 0;
+            int maxRows = chars.Count;
 
+            var targetChars = new List<char>() { '|', '-' };
 
+            while (row < maxRows)
+            {
+                if (targetChars.Contains(chars[row][col]))
+                {
+                    var features = new List<char>();
+                    features.Add(chars[row][col]);
+                    features.Add(chars[row + 1][col]);
+                    features.Add(chars[row + 2][col]);
+                    features.Add(chars[row + 3][col]);
 
+                    if (features.SequenceEqual(new List<char>() { '-', ' ', ' ', '-' }))
+                    {
+                        char number = '3';
+                        result.Add(number);
+                        col += 3;
+                    }
+
+                }
+                else if (chars[row][col] == '\n')
+                {
+                    row += 4;
+                }
+                else
+                {
+                    col++;
+                }
+            }
 
             return result;
         }
