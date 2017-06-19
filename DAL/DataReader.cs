132c5957652b06c207a46e1635b53a0d19d462e2
JulianChu <yulang.chu@gmail.com>
2017-06-19 11:38:18 +0200

    Rename class, and  finished DataReader

---
+++ DAL/DataReader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DAL
+{
+    public class DataReader
+    {
+
+        public string FilePath { get; set; }
+
+        public DataReader(string filePath)
+        {
+            this.FilePath = filePath;
+        }
+        public List<List<char>> GetData()
+        {
+            var result = new List<List<char>>();
+            result.Add(new List<char>());
+
+            string text = "";
+            try
+            {
+                text = File.ReadAllText(FilePath);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            char[] chars = text.ToCharArray();
+
+            int row = 0;
+            for (int i = 0; i < chars.Length; i++)
+            {
+                result[row].Add(chars[i]);
+                if (chars[i] == '\n')
+                {
+                    result.Add(new List<char>());
+                    row += 1;
+                }
+            }
+
+
+            return result;
+        }
+
+
+    }
+}
--- NumberParserExtended/Program.cs
+++ NumberParserExtended/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using DAL;
 
 namespace NumberParserExtended
 {
@@ -10,6 +7,19 @@ namespace NumberParserExtended
     {
         static void Main(string[] args)
         {
+            DataReader reader = new DataReader(@"c:\users\yulan\documents\visual studio 2015\Projects\NumberParserExtended\DAL\NumberParserExtended_Simplified.txt");
+            var result = reader.GetData();
+
+            foreach (var row in result)
+            {
+                foreach (var ch in row)
+                {
+                    Console.Write(ch);
+                }
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
