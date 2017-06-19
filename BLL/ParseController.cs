eb6b1ae80c50d932695ebe14d5a2be756df5982f
JulianChu <yulang.chu@gmail.com>
2017-06-19 11:49:39 +0200

    add a failed test case :  give 3 return 3

--- BLL/ParseController.cs
+++ BLL/ParseController.cs
@@ -1,7 +1,19 @@
-﻿namespace BLL
+﻿using System.Collections.Generic;
+
+namespace BLL
 {
     public class ParseController
     {
+        public List<char> ParseNumberFrom2DCharList(List<List<char>> chars)
+        {
+            var result = new List<char>();
+            int row = 0;
+            int col = 0;
 
+
+
+
+            return result;
+        }
     }
 }
---
+++ BLLTests/ParseControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BLL.Tests
+{
+    [TestClass()]
+    public class ParseControllerTests
+    {
+        [TestMethod()]
+        public void ParseNumberFrom2DCharList_Give_3_Return_3()
+        {
+            //Assign 
+            var data = new List<List<char>>()
+            {
+                new List<char>() { '-', '-', '-', '\r', '\n' },
+                new List<char>() { ' ', '/', ' ', '\r', '\n' },
+                new List<char>() { ' ', '\\', ' ', '\r', '\n' },
+                new List<char>() { '-', '-', ' ' }
+            };
+            var controller = new ParseController();
+
+            //Act
+            var actual = controller.ParseNumberFrom2DCharList(data);
+            //Assert
+            var expected = new List<char>() { '3' };
+            Assert.IsTrue(actual.SequenceEqual(expected));
+        }
+    }
+}
